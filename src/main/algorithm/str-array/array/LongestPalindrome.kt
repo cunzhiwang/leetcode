9f4facc704729ec243c41012689f2543715fc3a4
cunzhiwang <2539029477@qq.com>
2023-07-08 13:34:11 +0800

    feat: 最长回文字串

---
+++ src/main/algorithm/str-array/array/LongestPalindrome.kt
@@ -0,0 +1,34 @@
+package main.algorithm.`str-array`.array
+
+/**
+ * 最长回文子串
+ */
+fun longestPalindrome(s: String): String {
+    var start = 0
+    var end = 0
+
+    for (i in s.indices) {
+        val len1 = expandAroundCenter(s, i, i)
+        val len2 = expandAroundCenter(s, i, i + 1)
+        val len = maxOf(len1, len2)
+        if (len > end - start) {
+            start = i - (len - 1) / 2
+            end = i + len / 2
+        }
+    }
+
+    return s.substring(start, end + 1)
+}
+
+fun expandAroundCenter(s: String, left: Int, right: Int): Int {
+    var l = left
+    var r = right
+    while (l >= 0 && r < s.length && s[l] == s[r]) {
+        l--
+        r++
+    }
+    return r - l - 1
+}
+fun main() {
+println(longestPalindrome("babad"))
+}
