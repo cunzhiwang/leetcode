dab8968cc2eebb3adeff33103f71348620bcb8eb
cunzhiwang <2539029477@qq.com>
2023-07-10 22:07:22 +0800

    feat: 翻转单词

---
+++ src/main/algorithm/str-array/string/ReverseWords.kt
@@ -0,0 +1,16 @@
+package main.algorithm.`str-array`.string
+
+/**
+ * 翻转字符串里的单词
+ * 输入：s = "the sky is blue"
+ * 输出："blue is sky the"
+ */
+fun reverseWords(s: String): String {
+    // 去掉左右空格
+    var trim = s.trim()
+    return ""
+}
+fun main() {
+    reverseWords("")
+    println(" hello word  ".trim())
+}
