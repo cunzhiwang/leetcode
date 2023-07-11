64e639ebef73b56419564c99099de2227130c05f
cunzhiwang <2539029477@qq.com>
2023-07-11 10:38:08 +0800

    fix: 优化

---
+++ src/main/algorithm/str-array/string/LongestPalindrome.kt
@@ -0,0 +1,17 @@
+package algorithm.`str-array`.string
+
+/**
+ * 最长回文子串
+ *
+ * @author cunzhiwang
+ * @Date 2023/7/6 11:28
+ */
+fun longestPalindrome(s: String): String {
+    var left = 0
+    var right = s.length - 1
+
+    return ""
+}
+fun main() {
+
+}
