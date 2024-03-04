ba1284d3a263157c79dc1e153d5caa3df45e7470
cunzhiwang <2539029477@qq.com>
2024-03-04 22:37:46 +0800

    fix

--- src/main/algorithm/str-array/string/LongestCommonPrefix.kt
+++ src/main/algorithm/str-array/string/LongestCommonPrefix.kt
@@ -12,11 +12,15 @@ fun longestCommonPrefix(strs: Array<String>): String {
     for (i in 1 until strs.size) {
         while (strs[i].indexOf(prefix) != 0) {
             prefix = prefix.substring(0, prefix.length - 1)
-            if (prefix.isEmpty()) return ""
+            if (prefix.isEmpty()) {
+                return ""
+            }
         }
     }
     return prefix
 }
+
 fun main() {
-    longestCommonPrefix(arrayOf(""))
+    longestCommonPrefix(arrayOf("123", "12", "1"))
+    println("123".indexOf("23"))
 }
